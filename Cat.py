eaede71ef1086787a0afcfa341b2320eac7207bb
Afevraleff <afevraleff@ya.ru>
2022-03-20 14:27:09 +0200

    Initial commit

---
+++ Cat.py
@@ -0,0 +1,7 @@
+from main import Cat
+
+Baron = Cat('Baron', 'male', 2)
+Sam = Cat('Sam', 'male', 2)
+
+Baron.printCat()
+Sam.printCat()
---
+++ main.py
@@ -0,0 +1,8 @@
+class Cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+    def printCat(self):
+        print("Кот", self.name, "пол", self.gender, 'возраст', self.age)
+
